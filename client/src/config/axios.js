d2bf75579fe098f1aa1ee8c1d39072fc74f91ac2
Sagar Arora <sagararor403@gmail.com>
2023-11-04 15:12:16 +0530

    deployment

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -10,7 +10,7 @@ const axios = _axios.create({
     baseURL:
         import.meta.env.MODE != "development"
             ? "/api"
-            : "http://localhost:5000/api",
+            : "https://todo-list-api-mbmm.onrender.com/api",
     timeout: 3000,
     headers: {
         Accept: "application/json",
